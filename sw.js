b6eb1da9c9c58522cc77a7e7658e9f8e75e54d9d
doomz2k <doomz2k@googlemail.com>
2026-05-22 16:35:25 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,11 @@
-const VERSION = '1.0.14';
-
+// This service worker unregisters itself and clears all caches
 self.addEventListener('install', () => self.skipWaiting());
-
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
-});
-
-self.addEventListener('fetch', e => {
-  if(e.request.url.startsWith(self.location.origin)){
-    e.respondWith(
-      fetch(new Request(e.request, { cache: 'no-store' }))
-        .catch(() => caches.match(e.request))
-    );
-  }
 });
